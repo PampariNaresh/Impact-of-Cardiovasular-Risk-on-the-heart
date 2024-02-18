6ec32eb271043fee6f999c11b64d15882092a1a3
PampariNaresh <pamparinaresh2001@gmail.com>
2024-02-18 14:47:44 +0530

    Project Upload

---
+++ app.py
@@ -0,0 +1,177 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import os
+
+app = Flask(__name__)
+
+
+
+
+strokefilename = 'strokemodel.pkl'
+strokefilename =os.path.join(os.path.dirname(os.path.abspath(strokefilename)),strokefilename)
+strokemodel = pickle.load(open(strokefilename, 'rb'))
+
+heartfilename = 'heartmodel.pkl'
+heartfilename  =os.path.join(os.path.dirname(os.path.abspath(heartfilename)),heartfilename )
+heartmodel = pickle.load(open(heartfilename, 'rb'))
+
+@app.route("/")
+def home():
+  return render_template('home.html')
+
+
+@app.route('/heartdisease', methods=['GET','POST'])
+def heartdisease():
+    if request.method == 'POST':
+        age = int(request.form['age'])
+        sex = request.form.get('gender')
+        cp = request.form.get('chestpaintype')
+        trestbps = int(request.form['trestbps'])
+        chol = int(request.form['serumcholesterol'])
+        fbs = request.form.get('fastbloodsuger')
+        restecg = int(request.form['restingegc'])
+        thalach = int(request.form['maxheartrate'])
+        exang = request.form.get('exerciseinducedanginal')
+        oldpeak = float(request.form['oldpeak'])
+        slope = request.form.get('slope')
+        ca = int(request.form['ca'])
+        thal = request.form.get('thal')
+        
+        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        my_prediction = heartmodel.predict(data)
+       
+        return render_template('result.html', prediction=my_prediction)
+    elif request.method == 'GET':
+        return render_template('heartdiease.html')
+    else:
+        return render_template('home.html') 
+    
+@app.route('/strokeprediction', methods=['GET','POST'])
+def strokeprediction():   
+    if request.method == 'POST':
+        age = int(request.form['age'])
+        gender = float(request.form.get('gender'))
+        gender_encoded_Female=0
+        gender_encoded_Male=0
+        gender_encoded_Other=0
+        if(gender==1):
+            gender_encoded_Male=1
+        elif(gender==0):
+            gender_encoded_Female=1
+        elif(gender==2):
+             gender_encoded_Other=1
+
+        hypertension = int(request.form.get('hypertension'))
+        heart_disease = int(request.form['heartdisease'])
+        ever_married = int(request.form['marriedstatus'])
+        worktype =int( request.form.get('worktype'))
+        
+        work_type_encoded_Govt_job=0
+        work_type_encoded_Never_worked=0
+        work_type_encoded_Private=0
+        work_type_encoded_Self_employed=0
+        work_type_encoded_children=0
+        if(worktype==0):
+            work_type_encoded_children=1
+        elif(worktype==1):
+            work_type_encoded_Govt_job=1
+        elif(worktype==2):
+            work_type_encoded_Never_worked=1
+        elif(worktype==3):
+             work_type_encoded_Private=1
+        elif(worktype==4):
+            work_type_encoded_Self_employed=1
+            
+       
+
+        Residence_type = int(request.form['residencetype'])
+        avg_glucose_level = int(request.form['gloucoselevel'])
+        bmi = int(request.form.get('bodymassindex'))
+        smoking_status = int(request.form['smokingstatus'])
+        smoking_status_encoded_Unknown=0
+        smoking_status_encoded_formerly_smoked =0
+        smoking_status_encoded_never_smoked =0
+        smoking_status_encoded_smokes =0
+        if(smoking_status==0):
+            smoking_status_encoded_formerly_smoked =1
+        elif(smoking_status==1):
+            smoking_status_encoded_never_smoked =1
+        elif(smoking_status==2):
+              smoking_status_encoded_smokes =1
+        elif(smoking_status==3):
+             smoking_status_encoded_Unknown=1
+        
+        
+        
+        data = np.array([[age,
+        hypertension,
+        heart_disease,
+        ever_married,
+        Residence_type,
+        avg_glucose_level,
+        bmi,
+        gender_encoded_Female,
+        gender_encoded_Male,
+        gender_encoded_Other,
+        work_type_encoded_Govt_job,
+        work_type_encoded_Never_worked,
+        work_type_encoded_Private,
+        work_type_encoded_Self_employed,
+        work_type_encoded_children,
+        smoking_status_encoded_Unknown,
+        smoking_status_encoded_formerly_smoked,
+        smoking_status_encoded_never_smoked,
+        smoking_status_encoded_smokes]])
+        my_prediction = strokemodel.predict(data)
+        return render_template('strokeresult.html', prediction=my_prediction)
+    elif request.method == 'GET':
+        return render_template('strokeprediction.html')
+    else:
+        return render_template('home.html')    
+        
+        
+
+
+
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port='5000', debug=True)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('main.html')
+
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+
+        age = int(request.form['age'])
+        sex = request.form.get('sex')
+        cp = request.form.get('cp')
+        trestbps = int(request.form['trestbps'])
+        chol = int(request.form['chol'])
+        fbs = request.form.get('fbs')
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = request.form.get('exang')
+        oldpeak = float(request.form['oldpeak'])
+        slope = request.form.get('slope')
+        ca = int(request.form['ca'])
+        thal = request.form.get('thal')
+        
+        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        my_prediction = model.predict(data)
+        
+        return render_template('result.html', prediction=my_prediction)
+        
+        
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
---
+++ strokemodel.py
@@ -0,0 +1,191 @@
+# The libraries used in processing the dataset
+import numpy as np
+import pandas as pd
+
+from collections import Counter
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+from sklearn.preprocessing import StandardScaler
+
+
+filename =  '\datasets\heartstroke.csv'
+filename =os.getcwd() +filename
+strokedata = pd.read_csv(filename)
+
+#Removing id Column Because no use
+strokedata.drop("id", axis=1, inplace=True)
+def detect_outliers(df,features):
+    outlier_indices = []
+    
+    for c in features:
+        # 1st quartile
+        Q1 = np.percentile(df[c],25)
+        # 3rd quartile
+        Q3 = np.percentile(df[c],75)
+        # IQR
+        IQR = Q3 - Q1
+        # Outlier step
+        outlier_step = IQR * 1.5
+        # detect outlier and their indeces
+        outlier_list_col = df[(df[c] < Q1 - outlier_step) | (df[c] > Q3 + outlier_step)].index
+        # store indeces
+        outlier_indices.extend(outlier_list_col)
+    
+    outlier_indices = Counter(outlier_indices)
+    multiple_outliers = list(i for i, v in outlier_indices.items() if v > 2)
+    
+    return multiple_outliers
+
+
+#print(strokedata.loc[detect_outliers(strokedata,['age', 'avg_glucose_level', 'bmi', 'hypertension', 'heart_disease', 'stroke'])])
+# drop outliers
+strokedata = strokedata.drop(detect_outliers(strokedata,['age', 'avg_glucose_level', 'bmi', 'hypertension', 'heart_disease', 'stroke']),axis = 0).reset_index(drop = True)
+
+# print("Mean of BMI value for Females: ", np.mean(strokedata[strokedata['gender'] == 'Female']['bmi']))
+# print("Mean of BMI value for Males: ", np.mean(strokedata[strokedata['gender'] == 'Male']['bmi']))
+# print("Mean of BMI value for Others: ", np.mean(strokedata['bmi']))
+
+strokedata['bmi'] = strokedata['bmi'].fillna(0)
+for i in range(0,5035):
+    if(strokedata['bmi'][i] == 0):
+        if(strokedata['gender'][i] == 'Male'):
+            strokedata['bmi'][i] = 28.594683544303823
+        elif(strokedata['gender'][i] == 'Female'):
+            strokedata['bmi'][i] = 29.035926055109936
+        else:
+            strokedata['bmi'][i] = 28.854652338161664
+
+#print(len(strokedata[strokedata['bmi'].isnull()]))
+
+
+# from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+# print("Unique Values for Gender", dataset['gender'].unique())
+# print("Unique Values for ever_married", dataset['ever_married'].unique())
+# print("Unique Values for work_type", dataset['work_type'].unique())
+# print("Unique Values for Residence_type", dataset['Residence_type'].unique())
+# print("Unique Values for smoking_status", dataset['smoking_status'].unique())
+
+
+ever_married_mapping = {'No': 0, 'Yes': 1}
+strokedata['ever_married'] = strokedata['ever_married'].map(ever_married_mapping)
+
+Residence_type_mapping = {'Rural': 0, 'Urban': 1}
+strokedata['Residence_type'] = strokedata['Residence_type'].map(Residence_type_mapping)
+
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+onehotencoder = OneHotEncoder()
+strokedata['gender'] = pd.Categorical(strokedata['gender'])
+strokedataDummies_gender = pd.get_dummies(strokedata['gender'], prefix = 'gender_encoded')
+#strokedataDummies_gender
+
+strokedata['work_type'] = pd.Categorical(strokedata['work_type'])
+strokedataDummies_work_type = pd.get_dummies(strokedata['work_type'], prefix = 'work_type_encoded')
+#strokedataDummies_work_type
+
+strokedata['smoking_status'] = pd.Categorical(strokedata['smoking_status'])
+strokedataDummies_smoking_status = pd.get_dummies(strokedata['smoking_status'], prefix = 'smoking_status_encoded')
+#strokedataDummies_smoking_status
+
+strokedata.drop("gender", axis=1, inplace=True)
+strokedata.drop("work_type", axis=1, inplace=True)
+strokedata.drop("smoking_status", axis=1, inplace=True)
+
+strokedata = pd.concat([strokedata, strokedataDummies_gender], axis=1)
+strokedata = pd.concat([strokedata, strokedataDummies_work_type], axis=1)
+strokedata = pd.concat([strokedata, strokedataDummies_smoking_status], axis=1)
+
+
+#Model training
+features = ['age',
+ 'hypertension',
+ 'heart_disease',
+ 'ever_married',
+ 'Residence_type',
+ 'avg_glucose_level',
+ 'bmi',
+ 'gender_encoded_Female',
+ 'gender_encoded_Male',
+ 'gender_encoded_Other',
+ 'work_type_encoded_Govt_job',
+ 'work_type_encoded_Never_worked',
+ 'work_type_encoded_Private',
+ 'work_type_encoded_Self-employed',
+ 'work_type_encoded_children',
+ 'smoking_status_encoded_Unknown',
+ 'smoking_status_encoded_formerly smoked',
+ 'smoking_status_encoded_never smoked',
+ 'smoking_status_encoded_smokes']
+
+label = ['stroke']
+
+X = strokedata[features]
+y = strokedata[label]
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=3) 
+sc=StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import  accuracy_score
+
+
+max_accuracy =0
+best_x =0
+
+for x in range(100):
+    rf = RandomForestClassifier(random_state=x)
+    rf.fit(X_train,y_train.values.ravel())
+    Y_pred_rf = rf.predict(X_test)
+    current_accuracy = round(accuracy_score(Y_pred_rf,y_test)*100,2)
+    if(current_accuracy>max_accuracy):
+        max_accuracy = current_accuracy
+        best_x = x
+
+rf = RandomForestClassifier(random_state=best_x)
+rf.fit(X_train,y_train.values.ravel())
+import pickle
+
+with open('strokemodel.pkl','wb') as f:
+  pickle.dump(rf,f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
