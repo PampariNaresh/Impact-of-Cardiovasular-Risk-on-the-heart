ce9846c96e9fe56d67bd0c3d9e84beb62336f17c
PampariNaresh <pamparinaresh2001@gmail.com>
2024-02-18 15:27:20 +0530

    requirements

--- app.py
+++ app.py
@@ -145,43 +145,43 @@ def strokeprediction():
 
 
 
-if __name__ == "__main__":
-  #app.run(host='0.0.0.0', port='5000', debug=True)
-  app.run()
+# if __name__ == "__main__":
+#   #app.run(host='0.0.0.0', port='5000', debug=True)
+#   app.run()
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return render_template('main.html')
+# @app.route('/')
+# def home():
+# 	return render_template('main.html')
 
 
-@app.route('/predict', methods=['GET','POST'])
-def predict():
-    if request.method == 'POST':
+# @app.route('/predict', methods=['GET','POST'])
+# def predict():
+#     if request.method == 'POST':
 
-        age = int(request.form['age'])
-        sex = request.form.get('sex')
-        cp = request.form.get('cp')
-        trestbps = int(request.form['trestbps'])
-        chol = int(request.form['chol'])
-        fbs = request.form.get('fbs')
-        restecg = int(request.form['restecg'])
-        thalach = int(request.form['thalach'])
-        exang = request.form.get('exang')
-        oldpeak = float(request.form['oldpeak'])
-        slope = request.form.get('slope')
-        ca = int(request.form['ca'])
-        thal = request.form.get('thal')
+#         age = int(request.form['age'])
+#         sex = request.form.get('sex')
+#         cp = request.form.get('cp')
+#         trestbps = int(request.form['trestbps'])
+#         chol = int(request.form['chol'])
+#         fbs = request.form.get('fbs')
+#         restecg = int(request.form['restecg'])
+#         thalach = int(request.form['thalach'])
+#         exang = request.form.get('exang')
+#         oldpeak = float(request.form['oldpeak'])
+#         slope = request.form.get('slope')
+#         ca = int(request.form['ca'])
+#         thal = request.form.get('thal')
         
-        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
-        my_prediction = model.predict(data)
+#         data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+#         my_prediction = model.predict(data)
         
-        return render_template('result.html', prediction=my_prediction)
+#         return render_template('result.html', prediction=my_prediction)
         
         
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
 
