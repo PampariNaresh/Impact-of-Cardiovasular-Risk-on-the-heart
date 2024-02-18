348c9fe5979aaa4a2d38bc2e4d792e03f859d2df
PampariNaresh <pamparinaresh2001@gmail.com>
2024-02-18 15:23:12 +0530

    requirements

--- app.py
+++ app.py
@@ -146,7 +146,8 @@ def strokeprediction():
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port='5000', debug=True)
+  #app.run(host='0.0.0.0', port='5000', debug=True)
+  app.run()
 
 
 app = Flask(__name__)
